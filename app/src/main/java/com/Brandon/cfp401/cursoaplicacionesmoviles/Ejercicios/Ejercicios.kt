4204e4076bd494ff166306e09ae1ea1af5d22ec6
brandonbreiner <kiritoninjabrandon@gmail.com>
2025-05-30 17:19:33 -0300

    avances Array

--- app/src/main/java/com/Brandon/cfp401/cursoaplicacionesmoviles/Ejercicios/Ejercicios.kt
+++ app/src/main/java/com/Brandon/cfp401/cursoaplicacionesmoviles/Ejercicios/Ejercicios.kt
@@ -20,10 +20,26 @@ fun ejArray2B() {
         }
         println("La suma total es: $sumaParcial")
 }
+fun ejArray3() {
+    val numeros: Array<Int> = arrayOf(12, 4, 19, 33, 2, 7, 25, -8, 14, 6, 9, 3)
+    var minimo: Int = Int.MAX_VALUE
+    var maximo: Int = Int.MIN_VALUE
+    for(num in numeros) {
+        if (num < minimo) minimo = num
+        if (num > maximo) maximo = num
+    }
+        println("El número mínimo es: $minimo")
+        println("El número máximo es: $maximo")
 
 
+        }
 
-fun main() {
-    ejArray2B()
+fun ejArray3_0() {
+
 
 }
+
+
+ fun main () {
+   ejArray3_0()
+  }
